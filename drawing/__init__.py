5e56c0bd0e4e6a52cb9027c0be20c8b101fb8a74
jpsf24 <jpsf24@gmail.com>
2020-05-19 12:06:56 +0100

    Second commit

---
+++ drawing/__init__.py
@@ -0,0 +1,3 @@
+from drawing.colors import *
+from drawing.shapes import *
+from drawing.advanced.tools import *
---
+++ drawing/advanced/tools.py
@@ -0,0 +1,8 @@
+def pencil():
+    print("Vou escrever a lápis")
+
+def text():
+    print("Vou escrever a texto")
+
+def eraser():
+    print("Vou apagar com a borracha")
